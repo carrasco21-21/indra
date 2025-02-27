b15e031c39a076511aafecac924d73e49f06962e
Carrasco21 <pabloyluky@gmail.com>
2025-02-27 12:52:03 +0100

    insertar datos

--- src/indra/PrincipalApp.java
+++ src/indra/PrincipalApp.java
@@ -1,5 +1,12 @@
 package indra;
 
-public class PrincipalApp {
+import java.util.*;
 
+
+public class PrincipalApp {
+	public static final Scanner scanner = new Scanner(System.in);
+    public static final ArrayList<String> nombresEventos = new ArrayList<>();
+    public static final ArrayList<String> fechasEventos = new ArrayList<>();
+    public static final ArrayList<String> lugaresEventos = new ArrayList<>();
+    public static final HashMap<String, ArrayList<String>> inscripciones = new HashMap<>();
 }

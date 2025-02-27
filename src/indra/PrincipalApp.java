06e9da4f39b6a6394dc02713aab5b820de601631
Carrasco21 <pabloyluky@gmail.com>
2025-02-27 12:54:05 +0100

    rama main

--- src/indra/PrincipalApp.java
+++ src/indra/PrincipalApp.java
@@ -10,3 +10,31 @@ public class PrincipalApp {
     public static final ArrayList<String> lugaresEventos = new ArrayList<>();
     public static final HashMap<String, ArrayList<String>> inscripciones = new HashMap<>();
 }
+public static void main(String[] args) {
+    while (true) {
+        mostrarMenu();
+        int opcion = obtenerOpcion();
+        
+        switch (opcion) {
+            case 1:
+            	agregarEvento();
+            	break;
+            	
+            case 2 : 
+            	listarEventos();
+            	break;
+            case 3 :
+            	inscribirseEvento();
+            	break;
+            	
+            case 4:
+            	mostrarInscripciones();
+            	break;
+            case 5:
+            	salir();
+            	break;
+            default :
+            	System.out.println("Opción no válida. Intente de nuevo.");
+        }
+    }
+}
